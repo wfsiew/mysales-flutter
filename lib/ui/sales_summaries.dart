e0ee8bdeb85e1ccd3f8cee53d5e0db483f785045
Siew Wing Fei <siewwingfei@hotmail.com>
2019-08-26 15:11:56 +0800

    k

--- lib/ui/sales_summaries.dart
+++ lib/ui/sales_summaries.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:mysales_flutter/models/sales_summary.dart';
+import 'package:mysales_flutter/models/target.dart';
+import 'package:mysales_flutter/helpers/DBProvider.dart';
+import 'package:mysales_flutter/helpers/utils.dart';
 
 class SalesSummaries extends StatefulWidget {
-  SalesSummaries({Key key, this.title}) : super(key: key);
+  SalesSummaries({Key key, this.title, this.month, this.quarter, this.halfyear}) : super(key: key);
 
   final String title;
-  
+  final String month;
+  final String quarter;
+  final String halfyear;
 
   @override
   _SalesSummariesState createState() => _SalesSummariesState();
@@ -13,5 +19,40 @@ class SalesSummaries extends StatefulWidget {
 
 class _SalesSummariesState extends State<SalesSummaries> {
 
-  
+  DBProvider dbx;
+  Future<List<SalesSummary>> lf;
+
+  @override
+  void initState() {
+    super.initState();
+    dbx = DBProvider();
+    lf = load();
+  }
+
+  Future<List<SalesSummary>> load() async {
+    List<SalesSummary> ls = [];
+    Map<String, Target> mt;
+    await dbx.openDB();
+
+    var productGroups = await dbx.getProductGroups();
+    if (isNotEmpty(widget.month)) {
+      mt = await dbx.get
+    }
+
+    await dbx.closeDB();
+  }
+
+  Widget buildList() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: buildList(),
+    );
+  }
 }
